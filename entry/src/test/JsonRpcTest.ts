309b6779568a8f64307a046054dfc35715471118
AoEiuV020 <aoeiuv020@gmail.com>
2025-04-26 23:38:47 +0800

    封装层改成仅转换
    实际代码还都保持到外面，

--- entry/src/test/JsonRpcTest.ts
+++ entry/src/test/JsonRpcTest.ts
@@ -1,12 +1,7 @@
 import { JSONRPCClient } from 'json_rpc_2/src';
 
-export function createClient(): JSONRPCClient {
+export function createClient(foo: (obj: object) => Promise<void>): JSONRPCClient {
   return new JSONRPCClient((request) => {
-    try {
-      console.log('send request: ' + JSON.stringify(request));
-      return Promise.resolve();
-    } catch (error) {
-      return Promise.reject(error);
-    }
+    return foo(request);
   });
 }

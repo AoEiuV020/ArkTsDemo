e8a1a21d70258d4b9ced92d81742f133e6bf2092
AoEiuV020 <aoeiuv020@gmail.com>
2025-05-21 18:08:47 +0800

    调整Calc声明
    先声明空对象， 然后赋值其中的函数，

--- library/src/main/ets/calc/index.js
+++ library/src/main/ets/calc/index.js
@@ -3,12 +3,13 @@ import { add } from './add';
 import { decrease } from './decrease';
 import { decrease2 } from './decrease2';
 
-// 创建 Calc 对象
-export const Calc = {
-  minus: undefined, // 这将在 Index.ets 中被赋值
-  add,
-  decrease,
-  decrease2
-};
+// 创建空的 Calc 对象
+export const Calc = {};
+
+// 逐个函数赋值
+Calc.minus = undefined; // 这将在 Index.ets 中被赋值
+Calc.add = add;
+Calc.decrease = decrease;
+Calc.decrease2 = decrease2;
 
 console.log("Calc initialized:", Calc);
